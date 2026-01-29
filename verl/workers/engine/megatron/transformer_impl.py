a7fdbcdb14db71a4b6a4e5b17c47a071321f2af7
JacobHelwig <jacob.a.helwig@gmail.com>
2026-01-29 17:42:43 -0600

    megatron transformer impl

--- verl/workers/engine/megatron/transformer_impl.py
+++ verl/workers/engine/megatron/transformer_impl.py
@@ -26,7 +26,7 @@ from tensordict import TensorDict
 import verl.utils.torch_functional as verl_F
 from verl.models.mcore import get_mcore_forward_fused_no_padding_fn, get_mcore_weight_converter
 from verl.trainer.config import CheckpointConfig
-from verl.trainer.distillation import is_distillation_enabled
+from verl.trainer.distillation import is_distillation_enabled, compute_distillation_inputs
 from verl.utils import tensordict_utils as tu
 from verl.utils.checkpoint.megatron_checkpoint_manager import MegatronCheckpointManager
 from verl.utils.dataset.dataset_utils import DatasetPadMode
@@ -80,8 +80,6 @@ class MegatronEngine(BaseEngine):
         self.checkpoint_config = checkpoint_config
         self.distillation_config = distillation_config
         self.distillation_enabled = is_distillation_enabled(distillation_config)
-        if self.distillation_enabled:
-            raise NotImplementedError("Distillation is not supported yet in MegatronEngine")  # TODO: JacobHelwig
 
         assert self.engine_config.use_mbridge, "use_mbridge must be True"
         self._init_device_mesh()
@@ -697,7 +695,13 @@ class MegatronEngineWithLMHead(MegatronEngine):
         if calculate_entropy:
             entropy = output["entropy"]
             model_output["entropy"] = entropy
-
+        logits = output.get('logits')
+        cu_seqlens = logits.offsets() # TODO
+        model_output.update(
+            compute_distillation_inputs(
+                logits=logits, batch=data, cu_seqlens=cu_seqlens, config=self.distillation_config
+            )
+        )
         return model_output
 
     def forward_step(self, batch_iter: Iterator[TensorDict], model, postprocess_micro_batch_func):
