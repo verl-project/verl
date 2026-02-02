2d0d0e3c7734e5c9863e058935d289c32ca6b234
wenxie-amd <Wen.Xie@amd.com>
2026-02-02 13:59:50 +0800

    docker ci (#5)
    * docker ci
    
    * update
    
    * update dockerfile
    
    * update torch2.9 docker
    
    * rebuild te; patch vllm
    
    * rebuild te; patch vllm
    
    * rebuild te; patch vllm
    
    * rebuild te; patch vllm
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * run qwen3 30b on mi300x

--- tas/train/run_qwen3-30B-A3B_grpo.sh
+++ tas/train/run_qwen3-30B-A3B_grpo.sh
@@ -8,6 +8,7 @@ YOUR_RUN_NAME=r1-training_grpo-upstream
 TRAIN_PATH="$(cd "$(dirname "$0")"; pwd)"
 export TRAIN_PATH=$TRAIN_PATH
 export HF_HOME="/wekafs/0_public/huggingface"
+# export HF_HOME="/mnt/apps_proxy/tas/0_public/data/huggingface"
 export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 echo "TRAIN_PATH: $TRAIN_PATH"
 
@@ -19,6 +20,8 @@ export RAY_EXPERIMENTAL_NOSET_HIP_VISIBLE_DEVICES=1 # Patch with https://github.
 export VLLM_USE_FLASHINFER_MOE_FP8=0
 # TODO: aiter will fail on Qwen3-30B-A3B-Instruct model
 export VLLM_ROCM_USE_AITER=0
+export offload=True
+export optim_offload=${OFFLOAD_OPTIM:-True}
 
 # choose your model
 # MODEL_PATH=Qwen/Qwen2.5-0.5B-Instruct
@@ -46,12 +49,19 @@ python3 -m verl.trainer.main_ppo --config-path=config \
     data.max_response_length=1024 \
     actor_rollout_ref.model.path=$MODEL_PATH \
     actor_rollout_ref.actor.optim.lr=5e-7 \
-    actor_rollout_ref.actor.ppo_mini_batch_size=8 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=2 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.strategy=megatron \
     actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=1 \
     actor_rollout_ref.actor.megatron.tensor_model_parallel_size=1 \
     actor_rollout_ref.actor.megatron.expert_model_parallel_size=8 \
+    actor_rollout_ref.actor.megatron.param_offload=${offload} \
+    actor_rollout_ref.actor.megatron.optimizer_offload=${optim_offload} \
+    actor_rollout_ref.actor.megatron.grad_offload=${offload} \
+    actor_rollout_ref.actor.megatron.override_transformer_config.attention_backend='fused' \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
+    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=1 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -60,12 +70,13 @@ python3 -m verl.trainer.main_ppo --config-path=config \
     actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
-    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.rollout.n=4 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=40 \
     actor_rollout_ref.ref.strategy=megatron \
     actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=1 \
     actor_rollout_ref.ref.megatron.tensor_model_parallel_size=1 \
     actor_rollout_ref.ref.megatron.expert_model_parallel_size=8 \
+    actor_rollout_ref.ref.megatron.param_offload=${offload} \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=console \
