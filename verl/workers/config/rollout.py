9f351275a1d3e4a8fb1211999af0e4b91557a845
Guillem Tarrach <ggarciatarrach@gmail.com>
2026-02-11 16:02:09 +0100

    chore(misc): renamed some things

--- verl/workers/config/rollout.py
+++ verl/workers/config/rollout.py
@@ -117,7 +117,7 @@ class PrometheusConfig(BaseConfig):
     # Specify served_model_name to avoid displaying overly long model paths in Grafana
     served_model_name: Optional[str] = None
     # List of Prometheus metrics to query and log to experiment tracking
-    metrics_to_track: Optional[list[str]] = None
+    metrics_to_log: Optional[list[str]] = None
 
 
 @dataclass
