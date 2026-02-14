133e69e626b860f7564729d0a2cf36250e7454f8
JessicaZhong <jessicazhong@meta.com>
2026-02-13 17:42:26 -0800

    trigger ci

--- tests/special_e2e/sft/test_sft_engine_all.sh
+++ tests/special_e2e/sft/test_sft_engine_all.sh
@@ -37,7 +37,7 @@ BACKEND=megatron TP_SIZE=2 PP_SIZE=2 VPP_SIZE=${VPP_SIZE} CP_SIZE=2 NUM_GPUS=8 b
 echo "run with tp2 pp2 vpp2 cp2 num_gpus8 mode=ray"
 BACKEND=megatron TP_SIZE=2 PP_SIZE=2 VPP_SIZE=${VPP_SIZE} CP_SIZE=2 NUM_GPUS=8 mode=ray bash tests/special_e2e/sft/run_sft_engine.sh
 
-# Will add back torchtitan CI once everything is ready
+# TODO: Will add back torchtitan CI once everything is ready
 # # test with torchtitan fsdp=2
 # echo "run with tp1 pp1 cp1 fsdp2 num_gpus2"
 # BACKEND=torchtitan TP_SIZE=1 PP_SIZE=1 CP_SIZE=1 FSDP_SIZE=2 NUM_GPUS=2 bash tests/special_e2e/sft/run_sft_engine.sh
