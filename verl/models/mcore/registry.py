d9611d9f5c23ec57a0e4499b5859bb957381f824
Yan Bai <bayan@nvidia.com>
2026-02-25 19:36:18 -0800

    submit an RL script

--- verl/models/mcore/registry.py
+++ verl/models/mcore/registry.py
@@ -30,6 +30,7 @@ class SupportedVLM(Enum):
     QWEN2_5_VL = "Qwen2_5_VLForConditionalGeneration"
     QWEN3_MOE_VL = "Qwen3VLMoeForConditionalGeneration"
     QWEN3_VL = "Qwen3VLForConditionalGeneration"
+    QWEN3_5_MOE_VL = "Qwen3_5MoeForConditionalGeneration"
 
 
 supported_vlm = [member.value for member in SupportedVLM]
