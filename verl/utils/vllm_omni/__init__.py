96c5110ea77ef743c68441e2d05a12ea9cb0df60
knlnguyen1802 <knlnguyen1802@gmail.com>
2026-03-16 17:12:40 +0800

    Resolve and fix pre-commit
    Signed-off-by: knlnguyen1802 <knlnguyen1802@gmail.com>

--- verl/utils/vllm_omni/__init__.py
+++ verl/utils/vllm_omni/__init__.py
@@ -14,11 +14,6 @@
 
 from .utils import OmniTensorLoRARequest, VLLMOmniHijack
 
-# The contents of vllm/patch.py should not be imported here, because the contents of
-# patch.py should be imported after the vllm LLM instance is created. Therefore,
-# wait until you actually start using it before importing the contents of
-# patch.py separately.
-
 __all__ = [
     "OmniTensorLoRARequest",
     "VLLMOmniHijack",
