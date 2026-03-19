fc5c7f023e396d1605b932e40ba405e814bbbe45
yyyy2000 <yezhibei@huawei.com>
2026-03-19 14:08:54 +0800

    Update run_dapo_moonlight-16b_megatron_npu.sh

--- tests/special_npu/nightly_ci_ascend/run_dapo_moonlight-16b_megatron_npu.sh
+++ tests/special_npu/nightly_ci_ascend/run_dapo_moonlight-16b_megatron_npu.sh
@@ -187,4 +187,3 @@ python3 -m recipe.dapo.main_dapo \
     trainer.resume_mode=auto \
     trainer.log_val_generations=10 \
     trainer.total_training_steps=15 2>&1 | tee /root/.cache/nightly_log/moonlight/dapo_moonlight16b_megatron_npu-$(date +%Y%m%d_%H%M).log
-
