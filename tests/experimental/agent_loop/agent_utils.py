4f89bd8498e1ffe5f0f4579bbcbda6e7a8c447fc
Annarine <57247683+Annarine@users.noreply.github.com>
2026-04-07 10:28:07 +0800

    verl vllm-ascend yaml

--- tests/experimental/agent_loop/agent_utils.py
+++ tests/experimental/agent_loop/agent_utils.py
@@ -65,9 +65,7 @@ def init_agent_loop_manager(config: DictConfig) -> AgentLoopManager | RayWorkerG
     for resource_pool, class_dict in resource_pool_to_cls.items():
         worker_dict_cls = create_colocated_worker_cls(class_dict=class_dict)
         wg_dict = RayWorkerGroup(
-            resource_pool=resource_pool,
-            ray_cls_with_init=worker_dict_cls,
-            device_name=device_name
+            resource_pool=resource_pool, ray_cls_with_init=worker_dict_cls, device_name=device_name
         )
         spawn_wg = wg_dict.spawn(prefix_set=class_dict.keys())
         all_wg.update(spawn_wg)
