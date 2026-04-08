b562c231a1685b8f450a9c2f5ec55d5877d55724
autbuster <190498533@qq.com>
2026-04-08 02:11:04 +0000

    Bug fixes for the qwen3-235b model in 256k scenarios

--- examples/grpo_trainer/run_qwen3_235b_256k_megatron_npu.sh
+++ examples/grpo_trainer/run_qwen3_235b_256k_megatron_npu.sh
@@ -7,22 +7,23 @@
 ulimit -n 32768
 
 # Project Configuration
-project_name='GRPO-Qwen3-235B-A22B-BASE-MATH'
-exp_name='GRPO-Qwen3-235B-A22B-BASE-Megatron-vLLM'
+project_name='GRPO-Qwen3-235B-A22B-Instruct-MATH'
+exp_name='GRPO-Qwen3-235B-A22B-Instruct-Megatron-vLLM'
 
 # Node Info
 NNODES=${NNODES:-16}
 NPUS_PER_NODE=${NPUS_PER_NODE:-16}
 
 # Model Weights Paths
-MODEL_PATH=${WORK_DIR}/Qwen3-235B-A22B-Thinking-2507
+# MODEL_PATH=/mnt/weight/Qwen3-235B-A22B
+MODEL_PATH=${WORK_DIR}/Qwen3-235B-A22B-Instruct-2507
 MCORE_MODEL_PATH=${WORK_DIR}/Qwen3-235B-A22B-Instruct-2507-Mcore
 RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/verl"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
 
 # File System Paths
-TRAIN_FILE=${WORK_DIR}/dapo-math-17k.parquet
-TEST_FILE=${WORK_DIR}/dapo-math-17k.parquet
+TRAIN_FILE=${WORK_DIR}/gsm8k/train.parquet
+TEST_FILE=${WORK_DIR}/gsm8k/test.parquet
 
 # Data Configuration
 max_prompt_length=$((1024 * 1))
@@ -48,10 +49,10 @@ infer_ppo_max_token_len=$((max_prompt_length + max_response_length))
 optimizer_offload_fraction=1
 
 # Megatron Configuration
-train_tp=4
-train_ep=32
+train_tp=2
+train_ep=16
 train_etp=1
-train_pp=8
+train_pp=16
 train_cp=8
 
 # vLLM Configuration
@@ -63,37 +64,37 @@ max_model_len=$((max_prompt_length + max_response_length))
 max_num_batched_tokens=2048
 
 # Pipeline Layer Configuration
-first_layer=11
-last_layer=11
+first_layer=5
+last_layer=5
 
 # Data Configuration
 DATA_ARGS=(
-    data.train_files="${TRAIN_FILE}"
-    data.val_files="${TEST_FILE}"
-    data.prompt_key=prompt
-    data.train_batch_size=${train_prompt_bsz}
-    data.max_prompt_length=${max_prompt_length}
-    data.max_response_length=${max_response_length}
-    data.filter_overlong_prompts=False
-    data.truncation='left'
+     data.train_files="${TRAIN_FILE}"
+     data.val_files="${TEST_FILE}"
+     data.prompt_key=prompt
+     data.train_batch_size=${train_prompt_bsz}
+     data.max_prompt_length=${max_prompt_length}
+     data.max_response_length=${max_response_length}
+     data.filter_overlong_prompts=False
+     data.truncation='left'
 )
 
 # Model Configuration
 MODEL_ARGS=(
-    actor_rollout_ref.model.path="${MODEL_PATH}"
-    actor_rollout_ref.model.use_remove_padding=True
+     actor_rollout_ref.model.path="${MODEL_PATH}"
+     actor_rollout_ref.model.use_remove_padding=True
 )
 
 # RL Algorithm Configuration
 ALGORITHM_ARGS=(
-    algorithm.adv_estimator=${adv_estimator}
-    algorithm.use_kl_in_reward=${use_kl_in_reward}
-    algorithm.kl_ctrl.kl_coef=${kl_coef}
+     algorithm.adv_estimator=${adv_estimator}
+     algorithm.use_kl_in_reward=${use_kl_in_reward}
+     algorithm.kl_ctrl.kl_coef=${kl_coef}
 )
 
 # Actor Model Configuration
 ACTOR_ARGS=(
-    actor_rollout_ref.actor.strategy=megatron
+#     actor_rollout_ref.actor.strategy=megatron
     actor_rollout_ref.actor.use_torch_compile=False
     actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz}
     actor_rollout_ref.actor.use_kl_loss=${use_kl_loss}
@@ -104,6 +105,9 @@ ACTOR_ARGS=(
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${actor_ppo_max_token_len}
     actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz}
     actor_rollout_ref.actor.kl_loss_type=low_var_kl
+    actor_rollout_ref.actor.optim.clip_grad=1.0
+    actor_rollout_ref.actor.optim.lr_warmup_steps=10
+    actor_rollout_ref.actor.optim.weight_decay=0.1
     actor_rollout_ref.actor.optim.lr=1e-6
     +actor_rollout_ref.actor.optim.override_optimizer_config.optimizer_offload_fraction=${optimizer_offload_fraction}
     +actor_rollout_ref.actor.optim.override_optimizer_config.use_precision_aware_optimizer=True
@@ -157,6 +161,7 @@ REF_ARGS=(
 ROLLOUT_ARGS=(
     actor_rollout_ref.rollout.name=vllm
     actor_rollout_ref.rollout.n=${n_resp_per_prompt}
+    actor_rollout_ref.rollout.max_num_seqs=16
     actor_rollout_ref.rollout.top_p=1.0
     actor_rollout_ref.rollout.top_k=-1
     actor_rollout_ref.rollout.temperature=1.0
@@ -177,27 +182,27 @@ ROLLOUT_ARGS=(
 
 # Trainer Configuration
 TRAINER_ARGS=(
-    trainer.logger='["console","tensorboard"]'
-    trainer.project_name="${project_name}"
-    trainer.experiment_name="${exp_name}"
-    trainer.nnodes="${NNODES}"
-    trainer.n_gpus_per_node="${NPUS_PER_NODE}"
-    trainer.device='npu'
-    trainer.total_epochs=15
-    trainer.val_before_train=False
-    trainer.test_freq=-1
-    trainer.save_freq=-1
-    trainer.default_local_dir="${CKPTS_DIR}"
+     trainer.logger='["console","tensorboard"]'
+     trainer.project_name="${project_name}"
+     trainer.experiment_name="${exp_name}"
+     trainer.nnodes="${NNODES}"
+     trainer.n_gpus_per_node="${NPUS_PER_NODE}"
+     trainer.device='npu'
+     trainer.total_epochs=15
+     trainer.val_before_train=False
+     trainer.test_freq=-1
+     trainer.save_freq=-1
+     trainer.default_local_dir="${CKPTS_DIR}"
 )
 
 python3 -m verl.trainer.main_ppo \
-    --config-path=config \
-    --config-name='ppo_megatron_trainer.yaml' \
-    "${DATA_ARGS[@]}" \
-    "${MODEL_ARGS[@]}" \
-    "${ACTOR_ARGS[@]}" \
-    "${REF_ARGS[@]}" \
-    "${ROLLOUT_ARGS[@]}" \
-    "${ALGORITHM_ARGS[@]}" \
-    "${TRAINER_ARGS[@]}" \
-    "$@" | tee logs/run_qwen3moe-wy_235b_grpo_megatron_vllm_npu_$(date +%Y%m%d_%H%M%S).log
+     --config-path=config \
+     --config-name='ppo_megatron_trainer.yaml' \
+     "${DATA_ARGS[@]}" \
+     "${MODEL_ARGS[@]}" \
+     "${ACTOR_ARGS[@]}" \
+     "${REF_ARGS[@]}" \
+     "${ROLLOUT_ARGS[@]}" \
+     "${ALGORITHM_ARGS[@]}" \
+     "${TRAINER_ARGS[@]}" \
+     "$@" | tee logs/run_qwen3moe-wy_235b_grpo_megatron_vllm_npu_$(date +%Y%m%d_%H%M%S).log
