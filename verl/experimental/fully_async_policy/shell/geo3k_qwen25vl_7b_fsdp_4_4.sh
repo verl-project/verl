2dda0506504757678850578dd3b7921d9c46457f
huaiyizhao <huaiyizhao@tencent.com>
2026-04-13 20:14:15 +0800

    add rs is

--- verl/experimental/fully_async_policy/shell/geo3k_qwen25vl_7b_fsdp_4_4.sh
+++ verl/experimental/fully_async_policy/shell/geo3k_qwen25vl_7b_fsdp_4_4.sh
@@ -3,7 +3,7 @@ ENGINE=${1:-sglang}
 export WANDB_API_KEY=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
 
 
-# HF_MODEL_PATH=${HF_MODEL_PATH:-"${RAY_DATA_HOME}/models/Qwen2.5-VL-7B-Instruct"}
+HF_MODEL_PATH=${HF_MODEL_PATH:-"Qwen2.5-VL-7B-Instruct"}
 
 train_path=$HOME/data/geo3k/train.parquet
 test_path=$HOME/data/geo3k/test.parquet
@@ -21,15 +21,20 @@ NGPUS_PER_NODE=${NGPUS_PER_NODE:-8}
 n_gpus_rollout=4
 n_gpus_training=$((NGPUS_PER_NODE - n_gpus_rollout))
 
+max_prompt_length=1024
+max_response_length=8192
+actor_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 2))
+infer_ppo_max_token_len=$(((max_prompt_length + max_response_length) * 3))
+
 train_prompt_bsz=0
 gen_prompt_bsz=1
-n_resp_per_prompt=4
-train_prompt_mini_bsz=128
-total_rollout_steps=$(((512*100)))
+n_resp_per_prompt=16
+train_prompt_mini_bsz=64
+total_rollout_steps=$(((512*400)))
 test_freq=5
-staleness_threshold=0.1
-trigger_parameter_sync_step=4
-require_batches=2
+staleness_threshold=0.5
+trigger_parameter_sync_step=2
+require_batches=4
 partial_rollout=True
 total_epochs=200
 
@@ -43,15 +48,15 @@ python -m verl.experimental.fully_async_policy.fully_async_main \
     data.train_files="$train_path" \
     data.val_files="$test_path" \
     data.train_batch_size=${train_prompt_bsz} \
-    data.max_prompt_length=1024 \
-    data.max_response_length=2048 \
+    data.max_prompt_length=${max_prompt_length} \
+    data.max_response_length=${max_response_length} \
     actor_rollout_ref.rollout.max_model_len=32768 \
     actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.gen_batch_size=${gen_prompt_bsz} \
     data.return_raw_chat=${return_raw_chat} \
-    actor_rollout_ref.model.path=Qwen/Qwen3-VL-8B-Instruct \
+    actor_rollout_ref.model.path=${HF_MODEL_PATH} \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.hybrid_engine=False \
@@ -66,15 +71,16 @@ python -m verl.experimental.fully_async_policy.fully_async_main \
     actor_rollout_ref.actor.kl_loss_coef=0.01 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.grad_clip=1.0 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=5120 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${actor_ppo_max_token_len} \
     actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
-    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=5120 \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.ref.fsdp_config.param_offload=${ref_offload} \
     actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
-    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=5120 \
+    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.mode=${rollout_mode} \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
@@ -85,9 +91,9 @@ python -m verl.experimental.fully_async_policy.fully_async_main \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='verl_grpo_example_geo3k' \
     trainer.experiment_name='qwen2_5_vl_7b_fsdp_async' \
-    trainer.test_freq="${test_freq}" \
     trainer.total_epochs="${total_epochs}" \
-    trainer.val_before_train=False \
+    trainer.test_freq="${test_freq}" \
+    trainer.val_before_train=True \
     trainer.save_freq=-1 \
     trainer.resume_mode=auto \
     trainer.nnodes="${NNODES}" \
@@ -95,9 +101,9 @@ python -m verl.experimental.fully_async_policy.fully_async_main \
     rollout.nnodes="${NNODES}" \
     rollout.n_gpus_per_node="${n_gpus_rollout}" \
     rollout.total_rollout_steps="${total_rollout_steps}" \
-    trainer.total_epochs="${total_epochs}" \
-    trainer.test_freq="${test_freq}" \
     async_training.staleness_threshold="${staleness_threshold}" \
     async_training.trigger_parameter_sync_step="${trigger_parameter_sync_step}" \
     async_training.require_batches="${require_batches}" \
-    async_training.partial_rollout="${partial_rollout}"
+    async_training.partial_rollout="${partial_rollout}" \
+    algorithm.rollout_correction.rollout_rs=seq_mean_k1 \
+    algorithm.rollout_correction.rollout_rs_threshold="0.999_1.001"
