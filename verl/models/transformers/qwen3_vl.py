66c0fe5e6eb214336ae30fb1131c5da12b930fe3
ocp8 <144344752+ocp8@users.noreply.github.com>
2026-06-08 20:46:04 +0800

    Add vocab_weights device and shard handling for qwen3_vl.py
    Now qwen3_5 has a shard handling for vocab_weights(self.lm_head.weight) when it is a sharded tensor. Copy the same for qwen3_vl, and also solve the device problem when it is used with torch.compile which makes it as a cpu tensor by default

--- verl/models/transformers/qwen3_vl.py
+++ verl/models/transformers/qwen3_vl.py
@@ -364,11 +364,15 @@ def forward_with_torch_backend(
         rolled_labels = torch.roll(input_ids, shifts=-1, dims=-1)
     else:
         raise RuntimeError("To use forward_with_torch_backend, either labels or input_ids must be provided.")
+        
+    vocab_weights = self.lm_head.weight
+    if isinstance(vocab_weights, DTensor):
+        vocab_weights = vocab_weights.full_tensor().to(hidden_states.device)
 
     fused_linear_for_ppo = FusedLinearForPPO()
     log_probs, entropy = fused_linear_for_ppo.forward(
         hidden_states=hidden_states,
-        vocab_weights=self.lm_head.weight,
+        vocab_weights=vocab_weights,
         input_ids=rolled_labels,
         temperature=temperature,
     )
