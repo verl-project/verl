8f302682110c75953a9c4db062621e20b5558bbe
Begunner <went@bytedance.com>
2026-06-23 20:42:46 +0800

    add temperature

--- verl/workers/engine_workers.py
+++ verl/workers/engine_workers.py
@@ -337,6 +337,7 @@ class TrainingWorker(Worker, DistProfilerExtension):
             max_token_len_per_gpu=self.engine_config.max_token_len_per_gpu,
             micro_batch_size_per_gpu=self.engine_config.micro_batch_size_per_gpu,
             use_fused_kernels=self.engine_config.use_fused_kernels,
+            temperature=1.0,
         )
 
         for key, val in default_keys.items():
