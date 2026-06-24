c3d8cb6fb59ef55b9a9a0d1c4cebc20b56e78080
Begunner <went@bytedance.com>
2026-06-24 14:11:12 +0800

    fix ci

--- verl/utils/fsdp_utils.py
+++ verl/utils/fsdp_utils.py
@@ -731,7 +731,7 @@ def collect_lora_params(module: FSDP, layered_summon: bool, base_sync_done: bool
                     state_dict = {n: p for n, p in peft_model.named_parameters()}
                     lora_params = get_peft_model_state_dict(peft_model, state_dict=state_dict)
                     lora_params = {
-                        name: param.full_tensor().detach().cpu()
+                        name.replace("_fsdp_wrapped_module.", ""): param.full_tensor().detach().cpu()
                         if hasattr(param, "full_tensor")
                         else param.detach().cpu()
                         for name, param in lora_params.items()
