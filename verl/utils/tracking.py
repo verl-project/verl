5f5a375fa2331bf5ed520b08b5d357d96505eb47
Elia Palme <elia.palme@cscs.ch>
2026-06-25 10:43:06 +0200

    fix async train finish sequence

--- verl/utils/tracking.py
+++ verl/utils/tracking.py
@@ -185,7 +185,7 @@ class Tracking:
             if backend is None or default_backend in backend:
                 logger_instance.log(data=data, step=step)
 
-    def __del__(self):
+    def finish(self):
         if "wandb" in self.logger:
             self.logger["wandb"].finish(exit_code=0)
         if "swanlab" in self.logger:
@@ -200,6 +200,9 @@ class Tracking:
             self.logger["trackio"].finish()
         if "file" in self.logger:
             self.logger["file"].finish()
+
+    def __del__(self):
+        self.finish()
 
 
 class ClearMLLogger:
