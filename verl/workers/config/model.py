1125b682a0468241fadc33d6e43c133e3b38be36
huaye.zeng <huaye.zeng@bytedance.com>
2026-06-27 02:06:45 +0000

    format

--- verl/workers/config/model.py
+++ verl/workers/config/model.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from dataclasses import dataclass, field
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 from omegaconf import MISSING
 from transformers import AutoConfig
@@ -110,7 +110,8 @@ class HFModelConfig(BaseConfig):
     # custom chat template for the model
     custom_chat_template: Optional[str] = None
 
-    external_lib: Optional[str | list[str]] = None
+    # Need to skip formatting for below as otherwise OmegaConf can't resolve
+    external_lib: Optional[Union[str, list[str]]] = None  # noqa: UP007,UP045  # fmt: skip
 
     override_config: dict = field(default_factory=dict)
 
