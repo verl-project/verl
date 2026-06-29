3c6f4d622601df6911f0643c04837c1284466950
fh188 <773174225@qq.com>
2026-06-29 14:53:50 +0800

    fix fully async bug
    fix fully async bug

--- verl/workers/rollout/vllm_rollout/vllm_async_server.py
+++ verl/workers/rollout/vllm_rollout/vllm_async_server.py
@@ -1011,6 +1011,7 @@ class vLLMReplica(RolloutReplica):
                 # https://docs.vllm.ai/en/latest/usage/troubleshooting.html?h=nccl_cumem_enable#known-issues
                 # https://github.com/vllm-project/vllm/blob/c6b0a7d3ba03ca414be1174e9bd86a97191b7090/vllm/worker/worker_base.py#L445
                 "NCCL_CUMEM_ENABLE": "0",
+                "ASCEND_RT_VISIBLE_DEVICES": node_cuda_visible_devices,
                 "VLLM_ASCEND_AUTO_DETECT_QUANTIZATION": "0",
             }
             if os.environ.get("VLLM_ASCEND_TASK_QUEUE_ENABLE", None):
