199bda5cce04f6d1638736566a7b9d8e412a8b6a
Qi'an Zhou <zhouq@nvidia.com>
2026-08-17 14:39:06 +0800

    [tests] fix: use NVIDIA copyright headers

--- tests/utils/test_fsdp2_model_transfer_on_cpu.py
+++ tests/utils/test_fsdp2_model_transfer_on_cpu.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Bytedance Ltd. and/or its affiliates
+# Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
